e9685b70134f7ab6925cb50c4cf1717a79bb5688
aksdudghd <woojung0284@nate.com>
2019-06-20 16:29:29 +0900

    2019062005

--- frontend/src/components/PeoplePage.vue
+++ frontend/src/components/PeoplePage.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container">
     <section class="section">
-      <h2>조직도</h2>
+      <h2><strong>구성원</strong></h2>
       <div class="section-title">
         <h3></h3>
         <p></p>
-        <p>고급</p>
-        <p>중급</p>
-        <p>초급</p>
+        <p>CEO</p>
+        <p>기술이사</p>
+        <p>과장</p>
+        <p>대리</p>
+        <p>사원</p>
       </div>
     </section>
   </div>
